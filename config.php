638233abf00189a505243e01b8d47c115efc06da
Fidel <robertosjhs@gmail.com>
2026-06-02 09:11:58 +0200

    prueba

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
  
 define("DB_HOST","host");
-define("DB_NAME","gestion_usuarios");
+define("DB_NAME","funciona");
 define("DB_USER","root");
 define("DB_PASS","");
 
-$dsn=new pdo("mysql:host=".DB_HOST.";dbname=".DB_NAME);
+$dsn="mysql:host=".DB_HOST.";dbname=".DB_NAME;
 
-
+try {
+ $gbd=new pdo($dsn,DB_USER,DB_PASS);
+} catch (  $th) {
+    
+}
 
 
 
